87bd185b6f0d87e5c0b728ed7bfb86531b061d7a
Adam Grzegorowski <adam@tagoodies.com>
2015-07-20 20:51:17 +0200

    Updated docs.

--- SSDynamicText/SSDynamicButton.h
+++ SSDynamicText/SSDynamicButton.h
@@ -4,21 +4,20 @@
 //
 //  Created by Adam Grzegorowski on 18/07/15.
 //
-//
 
 #import <UIKit/UIKit.h>
 
 @interface SSDynamicButton : UIButton
 
 /**
- * Create a dynamic-sizing label that will adjust its size in response to changes
+ * Create a button with dynamic-sizing title label that will adjust its size in response to changes
  * to the user's preferred text size.
  */
 + (instancetype)buttonWithFont:(NSString *)fontName
                       baseSize:(CGFloat)size;
 
 /**
- * Create a dynamic-sizing label using a base font descriptor.
+ * Create a button with dynamic-sizing title label using a base font descriptor.
  */
 + (instancetype)buttonWithFontDescriptor:(UIFontDescriptor *)descriptor;
 
