e5bd71dae5a6fcb9654835676969f06bc61caec5
Remigiusz Herba <remigiusz.herba@allegro.pl>
2015-09-21 11:09:07 +0200

    [BUMP] 0.5.0

--- SSDynamicText/SSDynamicAttributedTextSizable.h
+++ SSDynamicText/SSDynamicAttributedTextSizable.h
@@ -11,7 +11,7 @@
 @protocol SSDynamicAttributedTextSizable <NSObject>
 
 /*
- TextView and TextField sometimes calls setAttributedText even when se work with normal Text. 
+ TextView and TextField sometimes calls setAttributedText even when we work with normal Text. 
  Framework is using it under the hood sometimes after layouts or even setText calls it. Because of that we cannot override
  default attributeText setter to change font, sometimes it change font at random.
  */
